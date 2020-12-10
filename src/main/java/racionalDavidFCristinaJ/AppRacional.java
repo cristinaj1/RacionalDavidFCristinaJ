b9f73feb949d9faa65e725ca04f0ea6927aa927d
cristinaj1 <cristinacee305@gmail.com>
2020-12-10 16:15:30 +0100

    Crear clase AppRacional y crear dos racionales

---
+++ src/main/java/racionalDavidFCristinaJ/AppRacional.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racionalDavidFCristinaJ;
+
+/**
+ *
+ * @author Cris
+ */
+public class AppRacional {
+    public static void main(String[] args) {
+        Racional z = new Racional();
+        Racional k = new Racional();
+        z.numerador=4;
+        z.denominador= 5;
+        k.numerador2= 3;
+        k.denominador2=4;
+        
+        
+    }
+    
+}
