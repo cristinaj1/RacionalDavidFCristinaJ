d8100883f29f5afbc8bcb6588c4bce4ba64b1325
David <fdezdavid144@gmail.com>
2020-12-10 13:52:45 +0100

    Matodos toString y Suma

--- src/main/java/racionalDavidFCristinaJ/Racional.java
+++ src/main/java/racionalDavidFCristinaJ/Racional.java
@@ -13,17 +13,27 @@ public class Racional {
 
     int numerador;
     int denominador;
+    int numerador2;
+    int denominador2;
 
     public Racional() {
         this.numerador = 1;
         this.denominador = 1;
+        this.numerador2 = 1;
+        this.denominador2 = 1;
     }
 
-    public Racional(int numerador, int denominador) {
+    public Racional(int numerador, int denominador, int numerador2, int denominador2) {
         this.numerador = numerador;
         this.denominador = denominador;
+        this.numerador2 = numerador2;
+        this.denominador2 = denominador2;
+
         if (denominador == 0) {
             denominador = 1;
+        }
+        if (denominador2 == 0) {
+            denominador2 = 1;
         }
     }
 
@@ -34,8 +44,37 @@ public class Racional {
     public int getDenominador() {
         return denominador;
     }
-    public void imprimirConsola(){
-        System.out.println("Numero racional: " + numerador + "/" + denominador);
-    
+
+    public int getNumerador2() {
+        return numerador2;
     }
+
+    public int getDenominador2() {
+        return denominador2;
+    }
+
+    public void imprimirConsola() {
+        System.out.println("Numero racional: " + numerador + "/" + denominador);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Racional{" + "a = " + numerador + ", b = " + denominador + '}';
+    }
+
+    public int Suma(int numerador, int denominador, int numerador2, int denominador2) {
+        int resultado = 0;
+        numerador = this.numerador;
+        denominador = this.denominador;
+        numerador2 = this.numerador2;
+        denominador2 = this.denominador2;
+        if (denominador == denominador2) {
+            resultado = numerador + numerador2;
+        } else {
+            resultado = (numerador * denominador2) + (numerador2 * denominador);
+        }
+        return resultado;
+    }
+
 }
